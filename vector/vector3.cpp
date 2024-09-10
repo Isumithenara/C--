344a34b6ef7391c8d3d84108e904eb0d740f80fa
Isumithenara <isumithenara2@gmail.com>
2024-09-10 21:53:25 +0530

    modified:   vector/vector3.cpp 	new file:   vector/vector3.exe 	new file:   vector/vectors4.cpp

--- vector/vector3.cpp
+++ vector/vector3.cpp
@@ -2,7 +2,34 @@
 #include <vector>
 using namespace std;
 int main()
-{
-    vector<int> v;
-    
+{//begin and end
+    // vector<int> v={2,3,5,6,23,5};
+    // vector<int>:: iterator it;
+    // for(it=v.begin();it != v.end(); it++)
+    // {
+    //     cout<<*it<<"  ";
+    // }
+//reverse begin and reverse end iterator   
+    //  vector<int> v={2,3,5,6,23,5};
+    // vector<int>:: reverse_iterator rit;
+    // for(rit=v.rbegin();rit != v.rend(); rit++)
+    // {
+    //     cout<<*rit<<"  ";
+    // }
+
+    //size() max size capacity
+    vector<char> vowels={'a','e','i','o'};
+    cout<<"size of vector is "<<vowels.size()<<endl;
+     cout<<" max size of vector is "<<vowels.max_size()<<endl;
+     cout<<"capacity of vector is "<<vowels.capacity()<<endl;
+    vowels.push_back('u');
+    cout<<"size of vector is "<<vowels.size()<<endl;
+     cout<<" max size of vector is "<<vowels.max_size()<<endl;
+     cout<<"capacity of vector is "<<vowels.capacity()<<endl;
+    vowels.resize(3);
+    for(int i=0;i<vowels.size();i++)
+    {
+        cout<<vowels.at(i)<<" ";
+    }
+    return 0;
 }
