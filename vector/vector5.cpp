49946c541862394d47cad929d4ec6f2f55ebc2c0
Isumithenara <isumithenara2@gmail.com>
2024-09-12 18:12:22 +0530

    modified:   vector/vector5.cpp new file:   vector/vector5.exe modified:   vector/vector6.cpp new file:   vector/vector6.exe modified:   vector/vectors4.cpp new file:   vector/vectors4.exe

--- vector/vector5.cpp
+++ vector/vector5.cpp
@@ -4,4 +4,18 @@ using namespace std;
 int main()
 {
     //wap to swap values of vect1 and vect 2
+    vector<int> vect1={1,2,3,4,5};
+    vector<int> vect2={11,12,13,14,15};
+    vect1.swap(vect2);
+    for(auto n:vect1)
+    {
+        cout<<n<<"  "<<endl;
+    }
+    for(auto n:vect2)
+    {
+      cout<<n<<"  ";
+    }
+
+return 0;
+
 }
--- vector/vector6.cpp
+++ vector/vector6.cpp
@@ -1,7 +1,15 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 int main()
 {
     //wap to sort vect1
+    vector<int> vect1={10,2,-1,4,79,60};
+    sort(vect1.begin(),vect1.end());
+   for (auto n: vect1)
+   {
+    cout<<n<<" ";
+   }
+   
 }
