fface75d3e5e8648096b600b722ffb83b7ea743b
Isumithenara <isumithenara2@gmail.com>
2024-10-02 23:16:17 +0530

    modified:   strings/s5.cpp 	new file:   strings/s5.exe

--- strings/s5.cpp
+++ strings/s5.cpp
@@ -3,5 +3,12 @@
 using namespace std;
 int main()
 {
-    
+    string full_name("isumithenara");
+    string f_name={full_name,0,5};
+    string l_name=full_name.substr(5,7);
+    string  formatted_full_name=f_name+l_name;
+    formatted_full_name.insert(5,"  ");
+    cout<<formatted_full_name;
+
+    return 0;
 }
