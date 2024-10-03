9848e014454d29009e9228d2b2c9f35f9962cda3
Isumithenara <isumithenara2@gmail.com>
2024-10-03 23:36:08 +0530

    modified:   strings/s6.cpp modified:   strings/s6.exe

--- strings/s6.cpp
+++ strings/s6.cpp
@@ -5,5 +5,27 @@ int main()
 //substitution cipher
 
 {
-cout<<"hi";
+   string encrypted_msg;
+   string alphabet{"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+   string key{"ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba"};
+   string msg{};
+   cout<< "HELLO THERE,ENTER YOUR MESSAGE";
+   getline(cin,msg);
+   cout<<endl<<"<<<....encrypting messge..<<<"<<endl;
+   for(char c:msg)
+   {
+    size_t position=alphabet.find(c);
+    if(position != string::npos)
+    {
+      char new_c=  key.at(position);
+      encrypted_msg += c;
+    }
+    else
+    {
+        encrypted_msg += c;
+    }
+   
+   }
+    cout<<"ur msg is "<<encrypted_msg;
+   return 0;
 }
