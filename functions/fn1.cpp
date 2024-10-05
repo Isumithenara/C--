8e494fd66aed358433fe87256cb8fb6ca242a6ee
Isumithenara <isumithenara2@gmail.com>
2024-10-05 21:05:10 +0530

    modified:   functions/fn1.cpp 	new file:   functions/fn1.exe 	new file:   functions/fun2.cpp

--- functions/fn1.cpp
+++ functions/fn1.cpp
@@ -1,1 +1,22 @@
+//defining  function
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+    int a,b,sum=0;
+   // cout<<"enter a b values";
+   // cin>>a>>b;
+    sum=a+b;
+    cout<<sum;
+    return a+b;//optional
+}
+int main()
+{
+  sum(3,4);
+  return 0;
+}
 
+void display(std::string name)
+{
+    cout<<"hi"<<name;
+}
